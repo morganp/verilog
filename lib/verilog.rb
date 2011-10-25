9b27c3c59c61f0bb8b80745db76623cc938cbb7d
Morgan Prior <morgan@amaras-tech.co.uk>
2011-10-25 22:37:22 +0100

    Increment version number for next release

--- lib/verilog.rb
+++ lib/verilog.rb
@@ -3,7 +3,7 @@ libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 module Verilog
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
 end
 
 require 'verilog/file'
