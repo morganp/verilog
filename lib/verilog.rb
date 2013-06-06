b6161677b8860d01ed899fc077bee8611b35a6c1
Morgan Prior <morgan@amaras-tech.co.uk>
2013-06-06 06:25:33 +0100

    Version Number bump for new feature

--- lib/verilog.rb
+++ lib/verilog.rb
@@ -3,7 +3,7 @@ libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 module Verilog
-  VERSION = '0.0.5'
+  VERSION = '0.1.0'
 end
 
 require 'verilog/file_list'
